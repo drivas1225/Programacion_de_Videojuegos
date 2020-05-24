40597e7b454dd798f923143ac049c7dbfaba0ee1
JRod166 <jose.vilca.vargas@gmail.com>
2020-05-24 18:47:57 -0500

    powerups

--- Bomberman/Assets/DinasourMovement.cs
+++ Bomberman/Assets/DinasourMovement.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class DinasourMovement : MonoBehaviour
 {
@@ -8,29 +9,62 @@ public class DinasourMovement : MonoBehaviour
 
     public Rigidbody2D rb;
     public Animator animator;
-
-    Vector2 movment;
+    public Tilemap tilemap;
+    public Tile wallTile;
+    public Tile destructibleTile;
+    public float horizontal = 1.0f;
+    public float vertical = 0.0f;
+    public Vector2 movment;
+    public Vector3 currentPosition;
     // Start is called before the first frame update
     void Start()
     {
-
+        currentPosition = new Vector3(transform.position.x, transform.position.y,transform.position.z);
     }
 
     // Update is called once per frame
     void Update()
     {
         //input
-        movment.x = Input.GetAxisRaw("Horizontal");
-        movment.y = Input.GetAxisRaw("Vertical");
+        Vector3Int currentPos = tilemap.WorldToCell(transform.position);
+        Tile tile;
+        tile = tilemap.GetTile<Tile>(currentPos + new Vector3Int((int)horizontal, (int)vertical, 0));
+        if (currentPosition == transform.position )
+        {
+            List<Vector2> directions = new List<Vector2>();
+            tile = tilemap.GetTile<Tile>(currentPos + new Vector3Int(1, 0, 0));
+            if (tile != wallTile && tile != destructibleTile)
+            {
+                directions.Add(new Vector2(1.0f, 0f));
+            }
+            tile = tilemap.GetTile<Tile>(currentPos + new Vector3Int(-1, 0, 0));
+            if (tile != wallTile && tile != destructibleTile)
+            {
+                directions.Add(new Vector2(-1.0f, 0f));
+            }
+            tile = tilemap.GetTile<Tile>(currentPos + new Vector3Int(0, 1, 0));
+            if (tile != wallTile && tile != destructibleTile)
+            {
+                directions.Add(new Vector2(0f, 1.0f));
+            }
+            tile = tilemap.GetTile<Tile>(currentPos + new Vector3Int(0, -1, 0));
+            if (tile != wallTile && tile != destructibleTile)
+            {
+                directions.Add(new Vector2(0f, -1.0f));
+            }
 
+            movment = directions[Random.Range(0, directions.Count)];
+            horizontal = movment.x;
+            vertical = movment.y;
+        }
+        movment.x = horizontal;
+        movment.y = vertical;
+        rb.MovePosition(rb.position + movment * movSpeed * Time.deltaTime);
+        currentPosition = rb.transform.position;
         animator.SetFloat("Horizontal", movment.x);
         animator.SetFloat("Vertical", movment.y);
         animator.SetFloat("Speed", movment.sqrMagnitude);
     }
 
-    void FixedUpdate()
-    {
-        //Movement
-        rb.MovePosition(rb.position + movment * movSpeed * Time.deltaTime);
-    }
+    
 }
