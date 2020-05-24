b1a9620c8ce58f5f041cf528198ad5ffab38e09d
JRod166 <jose.vilca.vargas@gmail.com>
2020-05-24 15:21:20 -0500

    bombas

--- Bomberman/Assets/Scripts/BombSpawner.cs
+++ Bomberman/Assets/Scripts/BombSpawner.cs
@@ -14,10 +14,16 @@ public class BombSpawner : MonoBehaviour
         if (Input.GetKeyUp("space"))
         {
             //Vector3 worldPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Vector3 cellsize = tilemap.cellSize;
             Vector3 worldPos = transform.position;
             Vector3Int cell = tilemap.WorldToCell(worldPos);
+            Vector3 rayStart = tilemap.CellToWorld(cell) + new Vector3(cellsize[0] / 2, cellsize[1] / 2, 0);
+            RaycastHit2D hit = Physics2D.Raycast(rayStart, new Vector2(cellsize[0], cellsize[1]), 1);
             Vector2 cellCenterPos = tilemap.GetCellCenterWorld(cell);
-            Instantiate(bombPrefab, cellCenterPos, Quaternion.identity);
+            if (!hit.collider || hit.collider.tag != "Bomb")
+            {
+                GameObject bomba = Instantiate(bombPrefab, cellCenterPos, Quaternion.identity) as GameObject;
+            }
         }
     }
 }
