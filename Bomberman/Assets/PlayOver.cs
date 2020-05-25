c05994142b1a8e1eac7fd229ad99d36416789493
Diego Rivas <diego.rivas@ucsp.edu.pe>
2020-05-25 02:10:18 -0500

    gameover

---
+++ Bomberman/Assets/PlayOver.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayOver : MonoBehaviour
+{
+    private AudioManager aManager;
+    void Start()
+    {
+        aManager = FindObjectOfType<AudioManager>();
+        aManager.Play("Theme Music");
+    }
+}
